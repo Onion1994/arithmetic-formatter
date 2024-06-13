8b57cb9578c3a611bc91cfc44375f7712569d7eb
Renato Cipolla <renato.cipolla94@gmail.com>
2024-06-13 17:40:47 +0100

    add non digit operands error

--- arithmetic.py
+++ arithmetic.py
@@ -1,6 +1,11 @@
 def arithmetic_arranger(problems, show_answers=False):
     for problem in problems:
         problem_array = problem.split(" ")
-        if problem_array[1] not in ["+", "-"]:
+        first_operand = problem_array[0]
+        operator = problem_array[1]
+        second_operand = problem_array[2]
+        if operator not in ["+", "-"]:
             return "Error: Operator must be '+' or '-'."
+        if not first_operand.isdigit() or not second_operand.isdigit():
+            return "Error: Numbers must only contain digits."
     return problems
