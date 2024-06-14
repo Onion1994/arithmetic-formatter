503ddfb76057fff3de7ff3d82165117d83e64f5a
Renato Cipolla <renato.cipolla94@gmail.com>
2024-06-14 11:17:48 +0100

    add third line

--- arithmetic.py
+++ arithmetic.py
@@ -1,15 +1,18 @@
 def arithmetic_arranger(problems, show_answers=False):
     if len(problems) > 5:
         return "Error: Too many problems."
+    third_line = ''
     for problem in problems:
         problem_array = problem.split(" ")
         first_operand = problem_array[0]
         operator = problem_array[1]
         second_operand = problem_array[2]
+        dashes_length = len(max(problem_array, key = len)) + 2
         if operator not in ["+", "-"]:
             return "Error: Operator must be '+' or '-'."
         if not first_operand.isdigit() or not second_operand.isdigit():
             return "Error: Numbers must only contain digits."
         if len(first_operand) > 4 or len(second_operand) > 4:
             return "Error: Numbers cannot be more than four digits."
+        third_line += ('-' * dashes_length) + ('    ')
     return problems
